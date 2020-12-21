13e24cd1291af080cbb227dc2a97a4127397d770
sambhav <70996077+sambhav6486@users.noreply.github.com>
2020-12-21 21:47:14 +0530

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,10 +1,11 @@
 function reverse(array){
-    let b = [];
-    for(var i = 0; i < array.length; i++){
-        b.push(array.pop());
-    }   
-    return b;
-   }
-   
-   
-   console.log(reverse([1, 2 ,3 ,4]));
+ let b = [];
+ for(let i = array.length-1; i >=0; i--){
+     let c = array.pop();
+    b.push(c);
+ }   
+ return b;
+}
+
+
+console.log(reverse([1, 2 ,3 ,4, 5, 6, 7]));
